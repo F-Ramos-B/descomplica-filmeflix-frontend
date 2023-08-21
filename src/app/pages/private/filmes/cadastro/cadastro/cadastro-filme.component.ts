8c82f538e1f024ef43ab8fe745f5d1118b9b9df8
Francisco Ramos Bisneto <framos.df@gmail.com>
2023-08-21 16:21:32 -0300

    Ajustando cadastro de filmes

--- src/app/pages/private/filmes/cadastro/cadastro/cadastro-filme.component.ts
+++ src/app/pages/private/filmes/cadastro/cadastro/cadastro-filme.component.ts
@@ -1,15 +1,12 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SelectItem } from 'primeng/api';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { InclusaoBaseComponent } from 'src/app/components/shared/inclusao-base.component';
-import { EnumTipoUsuario } from 'src/app/enums/enum-tipo-usuario.model';
-import { BaseCrudEntity } from 'src/app/models/base-crud-entity';
 import { Filme } from 'src/app/models/filme';
-import { AdminService } from 'src/app/services/admin.service';
+import { FilmeService } from 'src/app/services/filme.service';
 import { GeneroService } from 'src/app/services/genero.service';
 import { PlataformaService } from 'src/app/services/plataforma.service';
-import { UsuarioService } from 'src/app/services/usuario.service';
 import { FormUtils } from 'src/app/utils/form-utils';
 
 
@@ -20,29 +17,26 @@ import { FormUtils } from 'src/app/utils/form-utils';
 })
 export class CadastroFilmeComponent extends InclusaoBaseComponent<Filme> implements OnInit {
 
-  idTipoFixo: number;
   generos$: Observable<Array<SelectItem<number>>>;
   plataformas$: Observable<Array<SelectItem<number>>>;
 
   formulario: FormGroup = new FormGroup({
     titulo: new FormControl(null, Validators.required),
     descricao: new FormControl(null, Validators.required),
-    linkImagem: new FormControl(null, [Validators.required, Validators.email]),
+    linkImagem: new FormControl(null, Validators.required),
     linkFilme: new FormControl(null, Validators.required),
     classificacaoIndicativa: new FormControl(null, [Validators.required, Validators.min(6), Validators.max(18)]),
-    generos: new FormControl(null, [Validators.required]),
-    plataforma: new FormControl(null, [Validators.required]),
+    generos: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+    plataforma: new FormControl(null, [Validators.required])
   });
 
   constructor(
     protected override injector: Injector,
     private plataformaService: PlataformaService,
     private generoService: GeneroService,
-    private adminService: AdminService,
-    private usuarioService: UsuarioService
+    private filmeService: FilmeService
   ) {
     super(injector);
-    this.idTipoFixo = this.navigationParams?.tipo;
   }
 
   ngOnInit(): void {
@@ -65,50 +59,25 @@ export class CadastroFilmeComponent extends InclusaoBaseComponent<Filme> impleme
   }
 
   cadastrar() {
-    this.usuarioService.incluir(this.formulario.value).subscribe(resposta => this.confirmarOperacao(resposta));
+    this.filmeService.incluir(this.formulario.value).subscribe(resposta => this.confirmarOperacao(resposta));
   }
 
   editar() {
-    const isEdicaoAdmin = EnumTipoUsuario.ADMIN.id === this.idTipoFixo;
-    const service = isEdicaoAdmin ? this.adminService : this.usuarioService;
-
-    service.editar(this.idEdicao, this.formulario.value).subscribe(resposta => this.confirmarOperacao(resposta));
+    this.filmeService.editar(this.idEdicao, this.formulario.value).subscribe(resposta => this.confirmarOperacao(resposta));
   }
 
   observarMudancaTipo() {
-    if (this.idTipoFixo) {
-      this.perfil.setValue(this.idTipoFixo);
-    }
-
     if (this.idEdicao) {
-      this.email.disable();
-      this.carregarUsuario();
+      this.carregarFilme();
     }
   }
 
-  carregarUsuario() {
-    this.usuarioService.obterPorId(this.idEdicao).subscribe(usuario => this.formulario.patchValue(usuario));
+  carregarFilme() {
+    this.filmeService.buscarPorId(this.idEdicao).subscribe(filme => this.formulario.patchValue(filme));
   }
 
   voltar() {
-    if (this.idTipoFixo) {
-      const rota = EnumTipoUsuario.ADMIN.id === this.idTipoFixo ? 'admin' : 'usuario';
-      this.router.navigate([rota]);
-    } else {
-      this.router.navigate(['login']);
-    }
-  }
-
-  get dataNascimento(): FormControl {
-    return this.formulario.get('dataNascimento') as FormControl;
-  }
-
-  get email(): FormControl {
-    return this.formulario.get('email') as FormControl;
-  }
-
-  get perfil(): FormControl {
-    return this.formulario.get('perfil') as FormControl;
+    this.router.navigate(['']);
   }
 
 }
